475f56fdcc367d9c458a22b7c2654fdbb12bec5f
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-08 11:31:45 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 08-Feb-2025/Same Differences 251264.py
@@ -0,0 +1,19 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    count = 0
+    freq = {}  
+
+    for i in range(n):
+        key = a[i] - (i + 1) 
+        if key in freq:
+            count += freq[key] 
+        
+        freq[key] = freq.get(key, 0) + 1
+
+    print(count)
