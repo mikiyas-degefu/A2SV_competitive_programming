92d08d8a200a254201cb05473a822d121f265971
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-11 19:29:24 +0300

    Created 'A - Assem's Secret Code' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 11-Feb-2025/A - Assem's Secret Code 257791.py
@@ -0,0 +1,10 @@
+# Problem: A - Assem's Secret Code - https://codeforces.com/gym/586622/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    if s.lower() == 'yes':
+        print('YES')
+    else:
+        print('NO')
