f02afcbb6132d466a16c51c082c2b4bc6fa338b4
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-11 19:29:45 +0300

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 11-Feb-2025/B - The special paintbrush 257792.py
@@ -0,0 +1,18 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    index_b = None
+    last_b = 0
+
+    for i,v in enumerate(s):
+        if v == 'B' and index_b == None:
+            index_b = i
+        if v == 'B':
+            last_b = i
+    
+    print(len(s[index_b:last_b+1]))
