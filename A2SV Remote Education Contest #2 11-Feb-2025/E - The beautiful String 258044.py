b1135b68f81b4e1c0f1afbb0da71ee60d3ae4b82
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-11 22:10:20 +0300

    Created 'E - The beautiful String' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 11-Feb-2025/E - The beautiful String 258044.py
@@ -0,0 +1,22 @@
+# Problem: E - The beautiful String - https://codeforces.com/gym/586622/problem/E
+
+n = int(input())
+
+for _ in range(n):
+    s = input()   
+    seq = '1100'
+    count = s.count(seq)
+    lst_word = list(s)
+
+    q_size = int(input())
+    for _ in range(q_size):
+        i,v = input().split()
+        i = int(i) - 1
+
+        bf_change = "".join(lst_word[max(0,i-3) : i+4]).count(seq)
+        lst_word[i] = v
+        af_change = "".join(lst_word[max(0,i-3) : i+4]).count(seq)
+
+        count+=af_change - bf_change
+
+        print('YES' if count > 0 else 'NO')
