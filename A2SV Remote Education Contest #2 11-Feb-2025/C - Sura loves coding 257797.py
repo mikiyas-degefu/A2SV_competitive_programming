83b8370a8c5401fdf266842e54b462454fa3ecbb
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-11 19:30:49 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 11-Feb-2025/C - Sura loves coding 257797.py
@@ -0,0 +1,19 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+n = int(input())
+s = input().strip()
+
+original = []
+
+for i in range(n-1, -1, -1):
+    char = s[i]
+
+    if len(original) % 2 == 0:
+        pos = len(original) // 2
+    else:
+        pos = len(original) // 2
+    original.insert(pos, char)
+
+
+original_word = ''.join(original)
+print(original_word)
