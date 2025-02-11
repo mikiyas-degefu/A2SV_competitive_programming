6d48a801197c34e5ec8eaf051147ec4ca747b1a4
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-11 19:30:12 +0300

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 11-Feb-2025/D - Mugisho and Rufeyda 257794.py
@@ -0,0 +1,15 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+n,t = list(map(int, input().split(' ')))
+
+result = 10**(n-1)
+if result % t == 0:
+    print(result)
+else:
+    diff = t - (result % t)
+    result+=diff
+    result = str(result)
+    if len(result) > n:
+        print(-1)
+    else:
+        print(result)
