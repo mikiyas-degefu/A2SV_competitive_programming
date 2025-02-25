190b56bc196727f199192e70e126396206376bf8
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-25 13:34:29 +0300

    Created 'Find the Number of Distinct Colors Among the Balls' by A2SV Hub

---
+++ 25-Feb-2025/Find the Number of Distinct Colors Among the Balls 282321.py
@@ -0,0 +1,23 @@
+# Problem: Find the Number of Distinct Colors Among the Balls - https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls/description/
+
+from collections import Counter
+
+class Solution:
+    def queryResults(self, limit: int, queries: list[list[int]]) -> list[int]:
+        res = []
+        ball_map = {}  
+        color_freq = Counter()  
+
+        for ball, color in queries:
+            if ball in ball_map:
+                old_color = ball_map[ball]
+                color_freq[old_color] -= 1
+                if color_freq[old_color] == 0:
+                    del color_freq[old_color]  
+
+            ball_map[ball] = color
+            color_freq[color] += 1
+
+            res.append(len(color_freq))
+
+        return res
