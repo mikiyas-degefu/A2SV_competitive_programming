db58964a3f777610e8a00135a9d737da6ab942dd
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-02-26 10:17:12 +0300

    Created 'Longest Consecutive Sequence' by A2SV Hub

---
+++ 26-Feb-2025/Longest Consecutive Sequence 283048.py
@@ -0,0 +1,17 @@
+# Problem: Longest Consecutive Sequence - https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        longest = 0
+
+        for num in nums_set:
+            if num - 1 not in nums_set:
+                length = 1
+
+                while num + length in nums_set:
+                    length+=1
+
+                longest = max(length, longest)
+        
+        return longest
