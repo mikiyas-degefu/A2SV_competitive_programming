787293eafc8104c728b77dbbe541b33cc7e68e2c
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-03-03 17:18:59 +0300

    Created 'Remove Comments' by A2SV Hub

---
+++ 03-Mar-2025/Remove Comments 289610.py
@@ -0,0 +1,31 @@
+# Problem: Remove Comments - https://leetcode.com/problems/remove-comments/
+
+class Solution(object):
+    def removeComments(self, source):
+        """
+        :type source: List[str]
+        :rtype: List[str]
+        """
+        res, buffer, block_comment_open = [], '', False
+        for line in source:
+            i = 0
+            while i < len(line):
+                char = line[i]
+    
+                if char == '/' and (i + 1) < len(line) and line[i + 1] == '/' and not block_comment_open:
+                    i = len(line) 
+                elif char == '/' and (i + 1) < len(line) and line[i + 1] == '*' and not block_comment_open:
+                    block_comment_open = True
+                    i += 1
+
+                elif char == '*' and (i + 1) < len(line) and line[i + 1] == '/' and block_comment_open:
+                    block_comment_open = False
+                    i += 1
+
+                elif not block_comment_open:
+                    buffer += char
+                i += 1
+            if buffer and not block_comment_open:
+                res.append(buffer)
+                buffer = ''
+        return res
