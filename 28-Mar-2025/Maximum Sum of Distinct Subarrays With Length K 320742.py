c5b86f751a842b8e0d98f7d07f89522bebcd713f
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-03-28 06:57:48 -0400

    Created 'Maximum Sum of Distinct Subarrays With Length K' by A2SV Hub

---
+++ 28-Mar-2025/Maximum Sum of Distinct Subarrays With Length K 320742.py
@@ -0,0 +1,28 @@
+# Problem: Maximum Sum of Distinct Subarrays With Length K - https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        unique = {}
+        left = 0
+        right = 0
+        max_sum = 0
+        win_sum = 0
+
+        while right < len(nums):
+            if nums[right] not in unique:
+                unique[nums[right]] = None
+                win_sum += nums[right]
+                right+=1
+            else:
+                unique.pop(nums[left])
+                win_sum -= nums[left]
+                left+=1
+
+            if len(unique) == k:
+                max_sum = max(max_sum, win_sum)
+                unique.pop(nums[left])
+                win_sum -= nums[left]
+                left+=1
+        
+        return max_sum
+            
