22738faa1a9587e7073209c9c88397e85eb987aa
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-01 21:32:07 +0300

    Created 'Ugly Number' by A2SV Hub

---
+++ 01-Apr-2025/Ugly Number 323517.py
@@ -0,0 +1,17 @@
+# Problem: Ugly Number - https://leetcode.com/problems/ugly-number/description/
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        while n > 1:
+            if n%2 == 0:
+                n/=2
+            elif n%3 == 0:
+                n/=3
+            elif n%5 == 0:
+                n/=5
+            else:
+                return False
+        
+        if n == 1:
+            return True
+        return False
