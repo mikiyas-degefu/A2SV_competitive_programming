7c49947049da2c6b2e0b6547a90a6052ebb4707f
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-03 20:39:52 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 03-Apr-2025/Sereja and Dima 325355.py
@@ -0,0 +1,22 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+cards = list(map(int, input().split()))
+
+left = 0
+right = n - 1
+result = [0,0]
+
+is_saraja = True
+for i in range(n):
+    max_card = max(cards[left], cards[right])
+    if is_saraja:
+        result[0] += max_card
+    else:
+        result[1] += max_card
+    is_saraja = not is_saraja
+    if cards[left] > cards[right]:
+        left += 1
+    else:
+        right -= 1
+print(result[0], result[1])
