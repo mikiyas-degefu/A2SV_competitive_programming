75b27f460b3d875f43802a0787519838f4fed396
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-05 19:34:28 +0300

    Created 'Day at the Beach' by A2SV Hub

---
+++ 05-Apr-2025/Day at the Beach 326871.py
@@ -0,0 +1,23 @@
+# Problem: Day at the Beach - https://codeforces.com/problemset/problem/599/C
+
+n = int(input())
+h = list(map(int, input().split()))
+
+left_max = [0] * n
+right_min = [0] * n
+
+
+left_max[0] = h[0]
+for i in range(1, n):
+    left_max[i] = max(left_max[i-1], h[i])
+
+
+right_min[n-1] = h[n-1]
+for i in range(n-2, -1, -1):
+    right_min[i] = min(right_min[i+1], h[i])
+
+count = 0
+for i in range(n - 1):
+    if left_max[i] <= right_min[i + 1]:
+        count += 1
+print(count + 1)
