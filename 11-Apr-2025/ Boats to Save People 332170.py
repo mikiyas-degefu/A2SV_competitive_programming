b43557deb24ee7b9b3df21805577ec8f7d2ad3a7
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-11 21:37:37 +0300

    Created ' Boats to Save People' by A2SV Hub

---
+++ 11-Apr-2025/ Boats to Save People 332170.py
@@ -0,0 +1,16 @@
+# Problem:  Boats to Save People - https://leetcode.com/problems/boats-to-save-people/description/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left = 0
+        right = len(people) - 1
+        min_carry = 0
+
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1  
+            right -= 1  
+            min_carry += 1 
+
+        return min_carry
