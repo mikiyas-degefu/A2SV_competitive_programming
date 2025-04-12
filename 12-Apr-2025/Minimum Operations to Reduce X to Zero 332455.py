1d4e2599664a27560f32494ddb7a1aaf36e9ff00
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-12 11:42:08 +0300

    Created 'Minimum Operations to Reduce X to Zero' by A2SV Hub

---
+++ 12-Apr-2025/Minimum Operations to Reduce X to Zero 332455.py
@@ -0,0 +1,22 @@
+# Problem: Minimum Operations to Reduce X to Zero - https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/description/
+
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        target = sum(nums) - x
+        cur_sum = 0
+        max_window = -1
+
+        l = 0
+
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+            while l <= r and cur_sum > target:
+                cur_sum -= nums[l]
+                l += 1
+
+            if cur_sum == target:
+                max_window = max(max_window, r - l + 1)
+            
+
+        return -1 if max_window == -1 else len(nums) - max_window
+
