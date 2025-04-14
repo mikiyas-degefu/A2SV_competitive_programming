062ec2be72f282efb11e5c76d6a916937d939e9a
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-14 22:35:12 +0300

    Created 'C - z-sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/C - z-sort 333958.py
@@ -0,0 +1,33 @@
+# Problem: C - z-sort - https://codeforces.com/gym/603156/problem/C
+
+n = int(input())
+a = list(map(int, input().split()))
+
+z_sorted = [0] * n
+
+i = 0
+r = n - 1
+is_min = True
+idx = 0
+while idx < n:
+    if is_min:
+        if a[i] <= a[r]:
+            z_sorted[idx] = a[i]
+            i+=1
+        else:
+            z_sorted[idx] = a[r]
+            r-=1
+        is_min = False
+    else:
+        if a[i] <= a[r]:
+            z_sorted[idx] = a[r]
+            r-=1
+        else:
+            z_sorted[idx] = a[i]
+            i+=1
+        is_min = True
+    
+    idx+=1
+
+
+print(*z_sorted)
