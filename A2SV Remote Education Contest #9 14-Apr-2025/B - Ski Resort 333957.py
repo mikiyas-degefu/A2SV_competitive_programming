8a8f4bb7686cd582b6ceafc04b00e99f3f7ceea5
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-14 22:35:02 +0300

    Created 'B - Ski Resort' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/B - Ski Resort 333957.py
@@ -0,0 +1,22 @@
+# Problem: B - Ski Resort - https://codeforces.com/gym/603156/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n, k, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    count = 0
+    length = 0
+
+    for i in range(n):
+        if a[i] <= q:
+            length += 1
+        else:
+            if length >= k:
+                count += (length - k + 1) * (length - k + 2) // 2
+            length = 0
+    
+    if length >= k:
+        count += (length - k + 1) * (length - k + 2) // 2
+    
+    print(count)
