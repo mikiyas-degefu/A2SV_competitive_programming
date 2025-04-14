44dcc86f7d609c2c7822d30d30f499187321256e
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-14 21:47:19 +0300

    Created 'D - Urbanization' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/D - Urbanization 333901.py
@@ -0,0 +1,23 @@
+# Problem: D - Urbanization - https://codeforces.com/gym/603156/problem/D
+
+n, n1, n2 = list(map(int, input().split()))
+a = list(map(int, input().split()))
+
+a.sort(reverse=True)
+a1 = 0
+a2 = 0
+i = 0
+
+if n2 > n1:
+    n1, n2 = n2, n1
+
+while i < n2:
+    a1 += a[i]
+    i += 1
+while i < n2 + n1:
+    a2 += a[i]
+    i += 1
+
+result = float((a1 / n2) + (a2 / n1))
+formatted_value = "{:.8f}".format(result)
+print(formatted_value)
