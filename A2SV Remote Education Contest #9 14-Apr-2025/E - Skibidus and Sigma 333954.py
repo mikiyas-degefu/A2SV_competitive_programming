17915462e69fcf2d77d8e914cc7abb0fbbdf759b
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-14 22:33:37 +0300

    Created 'E - Skibidus and Sigma' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/E - Skibidus and Sigma 333954.py
@@ -0,0 +1,27 @@
+# Problem: E - Skibidus and Sigma - https://codeforces.com/gym/603156/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    #number if array and length of each array
+    n, m = map(int, input().split())
+
+    arrays = []
+
+    for _ in range(n):
+        x = list(map(int, input().split()))
+        arrays.append(x)
+    
+
+    arrays.sort(key=lambda x: sum(x), reverse=True)
+
+    extended_arrays = []
+    for i in range(n):
+        extended_arrays.extend(arrays[i])
+    score = 0
+    cur_sum = 0
+
+    for x in extended_arrays:
+        cur_sum += x
+        score += cur_sum
+    print(score)
