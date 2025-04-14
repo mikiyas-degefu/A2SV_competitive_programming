85ec894575f1f837bff669e0cfc087fc8f117bc1
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-14 22:34:53 +0300

    Created 'A - The Rules of the Bus' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/A - The Rules of the Bus 333955.py
@@ -0,0 +1,26 @@
+# Problem: A - The Rules of the Bus - https://codeforces.com/gym/603156/problem/A
+
+t = int(input())
+
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+
+    l = a[0] - 1
+    r = a[0] + 1
+    i = 1
+    is_valid = True
+    while i < n:
+        if a[i] < l or a[i] > r:
+            is_valid = False
+            break
+        l = min(l,a[i] - 1) 
+        r = max(r,a[i] + 1)
+        i += 1
+    
+    if is_valid:
+        print("YES")
+    else:
+        print("NO")
