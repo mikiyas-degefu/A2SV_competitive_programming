e8af3553a4cc74c721dbf2f6329091d9fcbb0a3a
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-23 18:44:16 +0300

    Created 'C - The big bang theory -  The Stockholm Syndrome' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 23-Apr-2025/C - The big bang theory -  The Stockholm Syndrome 339570.py
@@ -0,0 +1,21 @@
+# Problem: C - The big bang theory -  The Stockholm Syndrome - https://codeforces.com/gym/604857/problem/C
+
+def solve(n,a):
+    a.sort()
+    prev = 0
+    diff = 0
+    min_val = a[0]
+
+    for i in range(n):
+        diff = a[i] - prev - 1
+        if diff > 0 and a[i] - diff > 0:
+            a[i] = a[i] - diff
+        if prev - a[i] > 1:
+            return prev + 1
+        prev = a[i]
+
+    return prev + 1
+
+n = int(input())
+a = list(map(int, input().split()))
+print(solve(n, a))
