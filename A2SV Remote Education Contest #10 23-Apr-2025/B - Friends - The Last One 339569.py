45b8b4079d916365705973da598ca666b3f1b31c
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-23 18:44:15 +0300

    Created 'B - Friends - The Last One' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 23-Apr-2025/B - Friends - The Last One 339569.py
@@ -0,0 +1,33 @@
+# Problem: B - Friends - The Last One - https://codeforces.com/gym/604857/problem/B
+
+
+def solve(n,m,a):
+    if n > m: return "NO"
+
+    a.sort(reverse=True)
+    space = 0
+    prev = 0
+
+
+    for num in range(n):
+        if prev - a[num] >= 0:
+            space += a[num] + 1
+        else:
+            space += ((a[num] * 2) + 1)
+
+        prev = a[num]
+    
+    if a[0] - a[-1] >= 0:
+        space -= a[-1]
+    if space > m:
+        return "NO"
+    else:
+        return "YES"
+
+
+
+t = int(input())
+for _ in range(t):
+    n,m = map(int, input().split())
+    a = list(map(int, input().split()))
+    print(solve(n,m,a))
