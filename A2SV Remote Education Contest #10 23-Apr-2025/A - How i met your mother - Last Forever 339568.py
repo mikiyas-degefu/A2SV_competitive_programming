727ac4c0b47a145e0be4a13fb4e3c64ec30b1b3c
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-23 18:44:03 +0300

    Created 'A - How i met your mother - Last Forever' by A2SV Hub

---
+++ A2SV Remote Education Contest #10 23-Apr-2025/A - How i met your mother - Last Forever 339568.py
@@ -0,0 +1,15 @@
+# Problem: A - How i met your mother - Last Forever - https://codeforces.com/gym/604857/problem/A
+
+from collections import Counter
+n = int(input())
+a = input()
+
+frq = Counter(a)
+
+result = ''
+
+if 'n' in frq:
+    print("1 " * frq['n'] , end='')
+
+if 'z' in frq:
+    print("0 " * frq['z'] )
