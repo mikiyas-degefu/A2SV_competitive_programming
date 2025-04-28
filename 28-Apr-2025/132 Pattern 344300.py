47dc04798d3e17a82448016a644b3167eea1f81b
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-04-28 22:21:05 +0300

    Created '132 Pattern' by A2SV Hub

---
+++ 28-Apr-2025/132 Pattern 344300.py
@@ -0,0 +1,25 @@
+# Problem: 132 Pattern - https://leetcode.com/problems/132-pattern/
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+
+        stack = []
+        cur_min = nums[0]
+
+        for num in nums[1:]:
+            while stack and stack[-1][0] <= num:
+                stack.pop()
+            
+            if stack  and num > stack[-1][1]:
+                return True
+            
+            stack.append([num, cur_min])
+            cur_min = min(cur_min, num)
+
+        
+
+        return False
+
+        
