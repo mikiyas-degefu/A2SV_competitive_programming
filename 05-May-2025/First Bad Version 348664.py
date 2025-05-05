dc333e084ef4f713ebc9c3a72071b44eac1abfa8
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-05-05 19:12:56 +0300

    Created 'First Bad Version' by A2SV Hub

---
+++ 05-May-2025/First Bad Version 348664.py
@@ -0,0 +1,19 @@
+# Problem: First Bad Version - https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = n
+
+        while l <= r:
+            m = (l + r) // 2
+            if not isBadVersion(m):
+                l = m + 1
+            else:
+                if isBadVersion(m-1):
+                    r = m - 1
+                else:
+                    return m
