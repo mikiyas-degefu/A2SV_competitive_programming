00619d1766394f004491044e82be4e13d05e7d49
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-05-15 22:21:31 +0300

    Created 'Validate Binary Search Tree' by A2SV Hub

---
+++ 15-May-2025/Validate Binary Search Tree 356682.py
@@ -0,0 +1,28 @@
+# Problem: Validate Binary Search Tree - https://leetcode.com/problems/validate-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(node, lower = float('-inf'), upper = float('inf')):
+            if not node:
+                return True
+            
+            if not (lower < node.val < upper):
+                return False
+            
+            if not dfs(node.left, lower, node.val):
+                return False
+            
+            if not dfs(node.right, node.val, upper):
+                return False
+            
+            return True
+            
+        return dfs(root)
+        
