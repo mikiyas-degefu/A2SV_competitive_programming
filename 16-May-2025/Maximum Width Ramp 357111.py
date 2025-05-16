ab4359b3e1472e7351d0817cb350fc421bb785e7
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-05-16 19:31:34 +0300

    Created 'Maximum Width Ramp' by A2SV Hub

---
+++ 16-May-2025/Maximum Width Ramp 357111.py
@@ -0,0 +1,21 @@
+# Problem: Maximum Width Ramp - https://leetcode.com/problems/maximum-width-ramp
+
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        n = len(nums)
+        indices_stack = []
+
+
+        for i in range(n):
+            if not indices_stack or nums[indices_stack[-1]] > nums[i]:
+                indices_stack.append(i)
+
+        max_width = 0
+
+        for j in range(n - 1, -1, -1):
+            while indices_stack and nums[indices_stack[-1]] <= nums[j]:
+                max_width = max(max_width, j - indices_stack[-1])
+            
+                indices_stack.pop()
+
+        return max_width
