b45acd1cc33eed59e81e164eb29d4ba1ccdf356d
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-08-05 21:36:02 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 05-Aug-2025/Unique Paths 382123.py
@@ -0,0 +1,17 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
+
+        def dp(i,j):
+
+            if i == 0 or j == 0:
+                return 1
+
+            if (i,j) not in memo:
+                memo[(i,j)] = dp(i-1, j) + dp(i, j-1)
+            return memo[(i,j)]
+
+        
+        return dp(m-1,n-1)
