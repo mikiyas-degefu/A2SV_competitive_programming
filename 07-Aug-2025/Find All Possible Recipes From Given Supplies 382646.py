b11c8311eb83e7914326bcabcc6e276fc0b2f6ff
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-08-07 21:53:15 +0300

    Created 'Find All Possible Recipes From Given Supplies' by A2SV Hub

---
+++ 07-Aug-2025/Find All Possible Recipes From Given Supplies 382646.py
@@ -0,0 +1,36 @@
+# Problem: Find All Possible Recipes From Given Supplies - https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        available_suplies = set(supplies)
+
+        #queue to process recipe indices
+        recipe_queue = deque(range(len(recipes)))
+        created = []
+        last_size = -1
+
+        while len(available_suplies) > last_size:
+            last_size = len(available_suplies)
+            queue_size = len(recipe_queue)
+
+            while queue_size > 0:
+                queue_size -= 1
+                idx = recipe_queue.popleft()
+
+                if all (ingredient in available_suplies for ingredient in ingredients[idx]):
+                    available_suplies.add(recipes[idx])
+                    created.append(recipes[idx])
+                else:
+                    recipe_queue.append(idx)
+        
+        return created
+
+
+
+
+        
+        
+        
+
+
+        
