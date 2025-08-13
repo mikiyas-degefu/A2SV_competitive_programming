e8253fa74fabf43bcb035b988a7c1570d0ed6bdb
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-08-13 20:48:58 +0300

    Created 'Solving Questions with Brainpower' by A2SV Hub

---
+++ 13-Aug-2025/Solving Questions with Brainpower 384685.py
@@ -0,0 +1,24 @@
+# Problem: Solving Questions with Brainpower - https://leetcode.com/problems/solving-questions-with-brainpower/
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+
+        memo = {}
+        def dp(i):
+            if i >= len(questions):
+                return 0
+            
+            if i in memo:
+                return memo[i]
+            
+            
+            memo[i] = max(
+                #skip question
+                dp(i + 1),
+                #solve question
+                questions[i][0] + dp(i + 1 + questions[i][1])
+            )
+
+            return memo[i]
+        
+        return dp(0)
