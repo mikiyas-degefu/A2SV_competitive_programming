81c82c1bfe2518dee7bd6109b66d44716e1c74db
Mikiyas Degefu <80261020+mikiyas-degefu@users.noreply.github.com>
2025-08-28 22:26:44 +0300

    Created 'Median of Two Sorted Arrays' by A2SV Hub

---
+++ 28-Aug-2025/Median of Two Sorted Arrays 389716.py
@@ -0,0 +1,18 @@
+# Problem: Median of Two Sorted Arrays - https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = nums1 + nums2
+
+        merged.sort()
+
+        total = len(merged)
+
+        if total % 2 == 1:
+            
+            return float(merged[total // 2])
+        else:
+
+            middle1 = merged[total // 2 - 1]
+            middle2 = merged[total // 2]
+            return (float(middle1) + float(middle2)) / 2.0
